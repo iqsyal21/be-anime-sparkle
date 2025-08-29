dc9717a7423340c6b09aaf7821f1ff3ba445d77b
iqsyal21 <iqsyal21@gmail.com>
2025-08-29 20:29:40 +0700

    fix file delete

--- src/server.js
+++ src/server.js
@@ -1,20 +1,16 @@
-import { connectDB } from "./config/db.js";
 import app from "./app.js";
-import { initCronJobs } from "./cron/cron.js";
+import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     try {
         await connectDB();
-
-        initCronJobs();
-
         app.listen(PORT, () => {
             console.log(`✅ Server running on http://localhost:${PORT}`);
         });
     } catch (err) {
-        console.error("❌ Gagal start server:", err);
+        console.error("❌ Gagal konek ke database. Server tidak berjalan.");
         process.exit(1);
     }
 };
